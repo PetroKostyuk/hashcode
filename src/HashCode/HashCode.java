2129fcc8048c4a258df8b2e6f324b0589009a0d1
hop <milicpav@fel.cvut.cz>
2018-02-20 21:09:15 +0100

    - milicpav in

--- src/HashCode/HashCode.java
+++ src/HashCode/HashCode.java
@@ -3,5 +3,6 @@ package HashCode;
 public class HashCode {
     public static void main(String[] args) {
         System.out.println("Helloo!!!");
+        System.out.println("Hop (milicpav) is in!");
     }
 }
