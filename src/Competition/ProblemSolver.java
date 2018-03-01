bb6ef06cead1ea77b4706bf288333beb4c670697
Michal Najman <michal.najman@databreakers.com>
2018-03-01 20:54:50 +0100

    car count

--- src/Competition/ProblemSolver.java
+++ src/Competition/ProblemSolver.java
@@ -16,6 +16,7 @@ public class ProblemSolver {
     public void solve(Data data){
         Collections.sort(data.rides);
         for (int car = 0; car < data.vehiclesCount; car++) {
+            log.log("Car: " + car + "/" + data.vehiclesCount);
             List<Integer> carRides = Plan.make(data);
             data.assignedRides.add(carRides);
         }
