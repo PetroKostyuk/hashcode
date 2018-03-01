8b7d4479bac3ebe63f5df0753b0622e288e1f7ab
Michal Najman <michal.najman@databreakers.com>
2018-03-01 20:18:01 +0100

    payoff complete

--- src/Competition/Payoff.java
+++ src/Competition/Payoff.java
@@ -5,11 +5,22 @@ import Framework.Logger;
 public class Payoff {
     static Logger logger = new Logger(true);
 
-    public static Integer compute(Data data, Ride ride, Pos position, int timestep) {
-        if (timestep > ride.lastStart) {
+    public static Integer compute(Data data, Ride ride, Pos carPosition, int timestep) {
+        int payoff = ride.dist;
+
+        if (timestep == ride.s) {
+            payoff += data.bonus;
+        }
+
+        int availableAtStart = Utils.dist(carPosition, ride.pos1) + timestep;
+        if (availableAtStart > ride.lastStart) {
             return null;
         }
 
-        return ride.dist +
+        if (availableAtStart < ride.s) {
+            payoff += ride.s - availableAtStart;
+        }
+
+        return payoff;
     }
 }
