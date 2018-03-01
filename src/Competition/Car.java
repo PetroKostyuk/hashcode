84aec315d3478a0fa06a8e3d80d5f4a0e13c544d
hop <milicpav@fel.cvut.cz>
2018-03-01 21:41:33 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/HashCode/HashCode.java

---
+++ src/Competition/Car.java
@@ -0,0 +1,22 @@
+package Competition;
+
+public class Car {
+    Pos position;
+    int timeAvailable;
+
+    public Car(){
+        position
+
+    }
+
+    public Car(Pos pos, int timeAvailable){
+        this.position = pos;
+        this.timeAvailable = timeAvailable;
+
+    }
+
+    public Ride findRide(Data data){
+        return null;
+    }
+
+}
