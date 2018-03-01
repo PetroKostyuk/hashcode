c7e43e55ce1868d3c9066ac5ee6edba7265dca87
Michal Najman <michal.najman@databreakers.com>
2018-03-01 20:07:08 +0100

    payoff

---
+++ src/Competition/Payoff.java
@@ -0,0 +1,11 @@
+package Competition;
+
+import Framework.Logger;
+
+public class Payoff {
+    static Logger logger = new Logger(true);
+
+    public static Integer compute(Ride ride, Pos position, int timestep) {
+        return null;
+    }
+}
